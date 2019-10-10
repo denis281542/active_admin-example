ac287bad7aeb7e5fa7a56e378c32c18877723c72
denis281542 <deniskuimov@mail.ru>
2019-10-10 15:31:13 +0500

    Add Publish

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -1,7 +1,13 @@
 ActiveAdmin.register Post do
   permit_params :title, :body, :published_at, :user_id
 
-  member_action :publish, method: :put do
+  action_item :publish, only: :show do
+    link_to 'Publish', publish_admin_post_path(post), method: :put if !post.published_at?
+  end 
 
+  member_action :publish, method: :put do
+    post = Post.find(params[:id])
+    post.update(published_at: Time.zone.now)
+    redirect_to admin_post_path(post)
   end
  end
