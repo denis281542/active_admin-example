93ffedabe7fb31db5a61f511fe9d8926fee96c74
denis281542 <deniskuimov@mail.ru>
2019-10-10 15:16:57 +0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
   Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  
   resources :posts
   resources :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
