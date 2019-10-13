4b0b722c9ee2176d15ca07a8f36b59f28a8d0706
denis281542 <deniskuimov@mail.ru>
2019-10-13 19:51:34 +0500

    Edit posts controller delete def post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,6 @@
 class PostsController < InheritedResources::Base
 
-  def create
-    post = Post.create!(post_params)
-    redirect_to post
-  end
-
-  private
+    private
 
     def post_params
       params.require(:post).permit(:title, :body, :published_at, :user_id, :content)
