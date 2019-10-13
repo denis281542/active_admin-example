97b253cfa8298cd8d5be6ec6f9724cebd36a032b
denis281542 <deniskuimov@mail.ru>
2019-10-13 19:50:07 +0500

    Add gem 'image_processing'

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,14 @@
 class PostsController < InheritedResources::Base
 
+  def create
+    post = Post.create!(post_params)
+    redirect_to post
+  end
+
   private
 
     def post_params
-      params.require(:post).permit(:title, :body, :published_at, :user_id)
+      params.require(:post).permit(:title, :body, :published_at, :user_id, :content)
     end
 
 end
